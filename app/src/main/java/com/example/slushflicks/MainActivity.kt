facd1a3d55d073dd582c014fb7dad1e685fd7ee8
codecameo <code.cameo92@gmail.com>
2020-04-21 16:28:22 +0600

    Initial commit

---
+++ app/src/main/java/com/example/slushflicks/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.slushflicks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import java.security.AccessController.getContext
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
