d6b0945dc1f5104a98c14cffb56957890a4b5eab
codecameo <code.cameo92@gmail.com>
2020-05-19 20:20:50 +0600

    Refactor version name of library

--- app/src/main/java/com/sifat/slushflicks/model/CollectionModel.kt
+++ app/src/main/java/com/sifat/slushflicks/model/CollectionModel.kt
@@ -1,9 +1,13 @@
 package com.sifat.slushflicks.model
 
+import com.google.gson.annotations.SerializedName
 import com.sifat.slushflicks.utils.EMPTY_STRING
 
 data class CollectionModel(
+    @SerializedName("name")
     var name: String = EMPTY_STRING,
+    @SerializedName("label")
     var label: String = EMPTY_STRING,
+    @SerializedName("isEnable")
     var isEnable: Boolean = false
 )
