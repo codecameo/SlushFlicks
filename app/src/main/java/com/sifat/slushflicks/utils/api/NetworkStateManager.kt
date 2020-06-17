b9ede5ec834b094973caf5f7df162d2d02e9f500
codecameo <code.cameo92@gmail.com>
2020-06-17 22:58:09 +0600

    Register network request callback

--- app/src/main/java/com/sifat/slushflicks/utils/api/NetworkStateManager.kt
+++ app/src/main/java/com/sifat/slushflicks/utils/api/NetworkStateManager.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Build
+import android.os.Build.VERSION_CODES.LOLLIPOP
 import androidx.annotation.RequiresApi
 
 
@@ -12,17 +14,38 @@ class NetworkStateManager(context: Context) {
 
     private var connectivityManager: ConnectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private var isOnline = false
+
+    init {
+        if (Build.VERSION.SDK_INT >= LOLLIPOP) {
+            val request = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+            val callback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    isOnline = checkNetworkState(network)
+                }
+            }
+            /**
+             * This wont cause memory leak as
+             * The app continues to receive callbacks until either the app exits or it calls unregisterNetworkCallback().
+             * */
+            connectivityManager.registerNetworkCallback(request, callback)
+        }
+    }
 
     fun isOnline(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkNetworkState(connectivityManager.activeNetwork)
+        return if (Build.VERSION.SDK_INT >= LOLLIPOP) {
+            isOnline
         } else {
             val networkInfo = connectivityManager.activeNetworkInfo
             networkInfo?.let { networkInfo.isConnected } ?: false
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(LOLLIPOP)
     fun checkNetworkState(activeNetwork: Network?): Boolean {
         val networkCapability = connectivityManager.getNetworkCapabilities(activeNetwork)
         return networkCapability?.let {
